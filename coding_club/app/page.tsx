7ade845aa94bfe216d2e937bbca8938a18c30b0b
coderDIVYANSH1705 <divyanshmishraspeaks@gmail.com>
2026-02-28 00:21:08 +0530

    terminal

--- coding_club/app/page.tsx
+++ coding_club/app/page.tsx
@@ -1,11 +1,13 @@
 import HeroScroller from './Components/Hero';
 import HeaderSection from './Components/HeaderSection';
 import CodingClubHeader from './Components/Header';
+import Terminal from './Components/Terminal';
 export default function Page() {
   return (
     <main>
       <HeaderSection />
       <HeroScroller />
+      <Terminal />
     </main>
   );
 }
