befd3e3c936e16927ac9979c9221f565d170089e
coderDIVYANSH1705 <divyanshmishraspeaks@gmail.com>
2026-03-11 23:38:52 +0530

    join club

--- coding_club/app/page.tsx
+++ coding_club/app/page.tsx
@@ -1,20 +1,20 @@
-import HeroScroller from './Components/Hero';
-import HeaderSection from './Components/HeaderSection';
-import CodingClubHeader from './Components/HeaderSection';
-import Terminal from './Components/Terminal';
-import AboutClub from './Components/About';
-import ClubTechStack from './Components/TechnologyArsenal';
-import TechCentre from './Components/TechCentre';
-import ScrollSequence from './Components/TechCentre';
-import EventGallery from './Components/EventGallery';
-import JoinClubForm from './Components/JoinClub';
+import HeroScroller from '../Components/Hero';
+import HeaderSection from '../Components/HeaderSection';
+import CodingClubHeader from '../Components/HeaderSection';
+import Terminal from '../Components/Terminal';
+import About3D from '@/Components/About';
+import ClubTechStack from '../Components/TechnologyArsenal';
+import TechCentre from '../Components/TechCentre';
+import ScrollSequence from '../Components/TechCentre';
+import EventGallery from '../Components/EventGallery';
+import JoinClubForm from '../Components/JoinClub';
 export default function Page() {
   return (
     <main>
       <HeaderSection />
       <HeroScroller />
       <Terminal />
-      <AboutClub />
+      <About3D />
       <ClubTechStack />
       <EventGallery />
       <JoinClubForm />
