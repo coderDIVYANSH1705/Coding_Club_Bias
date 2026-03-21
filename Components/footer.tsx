06c7f6b2fc329dbc2994ff0cbd6816737a43bf19
coderDIVYANSH1705 <divyanshmishraspeaks@gmail.com>
2026-03-21 15:22:00 +0530

    teams

--- Components/footer.tsx
+++ Components/footer.tsx
@@ -112,7 +112,7 @@ export default function Footer() {
           </p>
           <Link href="/components/team">
     <span className="text-[#00FF41] font-bold cursor-pointer hover:underline">
-      Divyansh & the Core Team
+      Develped By - Divyansh Mishra and Jayendra Nayal 
     </span>
   </Link>
         </div>
